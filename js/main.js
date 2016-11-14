9c9f48df6423fa6f3fe857e2f1d35b35cf116b83
iqbaladan <iqbal.adan@gmail.com>
2016-11-14 23:20:59 +0700

    modified js/main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+var newItemCounter = 1;
+var ourList = document.getElementById("our-list");
+var ourButton = document.getElementById("our-button");
+var ourHeadline = document.getElementById("our-headline");
 var listItems = document.getElementById("our-list").getElementsByTagName("li")
+
+for (i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", activateItem);
+}
+
+function activateItem() {
+     ourHeadline.innerHTML = this.innerHTML;
+}
+
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem() {
+     ourList.innerHTML += "<li>Something new " + newItemCounter + "</li>";
+     newItemCounter++;
+}
