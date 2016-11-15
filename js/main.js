531e62ccd38fc24872d32c80dcbab9a8a69d0959
Iqbal Adan <iqbal.adan@gmail.com>
2016-11-15 16:03:25 +0700

    modified js/main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 var newItemCounter = 1;
-var ourList = document.getElementById("our-list");
+var ourList = document.querySelector("#our-list");
 var ourButton = document.getElementById("our-button");
 var ourHeadline = document.getElementById("our-headline");
-var listItems = document.getElementById("our-list").getElementsByTagName("li")
+var listItems = document.querySelectorAll("#our-list li");
 
-for (i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", activateItem);
-}
+ourList.addEventListener("click", activateItem);
 
-function activateItem() {
-     ourHeadline.innerHTML = this.innerHTML;
+function activateItem(e) {
+    if (e.target.nodeName == "LI") {
+    ourHeadline.innerHTML = e.target.innerHTML;
+    for (i = 0; i < e.target.parentNode.children.length; i++) {
+      e.target.parentNode.children[i].classList.remove("active");
+	}
+	e.target.classList.add("active");
+    }
 }
 
 ourButton.addEventListener("click", createNewItem);
